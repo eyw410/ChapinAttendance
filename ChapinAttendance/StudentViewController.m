bc163e35c5e0f52b0e8c1562e08ec29a989a393a
eyw410 <eyw410@gmail.com>
2015-02-01 13:21:07 -0500

    picture

--- ChapinAttendance/StudentViewController.m
+++ ChapinAttendance/StudentViewController.m
@@ -26,6 +26,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"screenie.png"]]];
 
     NSLog(@"is this working");
     _locationManager = [[CLLocationManager alloc]init]; // initializing locationManager
