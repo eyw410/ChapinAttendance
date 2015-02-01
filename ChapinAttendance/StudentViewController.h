292239d9d9cf7b1f1eba8464807ab64a92c67790
2015lucyb <bermanlucy@yahoo.com>
2015-02-01 10:36:59 -0500

    added button

---
+++ ChapinAttendance/StudentViewController.h
@@ -0,0 +1,13 @@
+//
+//  StudentViewController.h
+//  ChapinAttendance
+//
+//  Created by Lucy Berman on 2/1/15.
+//  Copyright (c) 2015 Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StudentViewController : UIViewController
+
+@end
